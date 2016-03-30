355b17dde39cd9d562a0213e79044dd7c74fc257
Daniel Shamama <daniel.shama@gmail.com>
2016-03-30 14:37:24 +0300

    Update CheckList.h

--- MSWEproject/CheckList/CheckList.h
+++ MSWEproject/CheckList/CheckList.h
@@ -7,25 +7,24 @@ using namespace std;
 #pragma once
 class CheckList
 {
-	private:
-		int amount;
-		COORD coord;
-		string * options;
-		int width;
-		HANDLE handler;
-		boolean ifClicked;
-		vector<ItemList> items;
-		vector<int> isChecked;
-		vector<int> isClicked;
-		boolean checkClickedPosition(COORD dwMousePosition);
-		int whoClicked();
-	public:
-		CheckList(string opts[], int optAmount, short x, short y);
-		void handleInput(INPUT_RECORD iRecord);
-		void keyEventProc(KEY_EVENT_RECORD ker);
-		void MouseEventProc(MOUSE_EVENT_RECORD mer);
-		vector<int> whoChecked();
+private:
+	int amount;
+	COORD coord;
+	string * options;
+	int width;
+	HANDLE handler;
+	boolean ifClicked;
+	vector<ItemList> items;
+	vector<int> isChecked;
+	vector<int> isClicked;
+	boolean checkMousePosition(COORD dwMousePosition);
+	int whoClicked();
+public:
+	CheckList(string opts[], int optAmount, short x, short y);
+	void handleInput(INPUT_RECORD iRecord);
+	void keyEventProc(KEY_EVENT_RECORD ker);
+	void MouseEventProc(MOUSE_EVENT_RECORD mer);
+	vector<int> whoChecked();
 
-		~CheckList();
+	~CheckList();
 };
-
